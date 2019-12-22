4c38fe6727c3fa76f2876c01fdc778e8e70dbe4d
fanjunwei <fanjunwei003@163.com>
2019-12-22 15:37:39 +0800

    修改初始脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 mkdir -p ./files/usr/lib/vm_manager/
 if [[ ! -d "./files/usr/lib/vm_manager/vm_manager_vue" ]]; then
   git clone https://github.com/fanjunwei/vm_manager_vue.git ./files/usr/lib/vm_manager/vm_manager_vue
